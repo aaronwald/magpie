5084da44d6bce4c02076f223c164a5df13f512f8
Aaron Wald <aaronwald@gmail.com>
2024-11-30 18:27:07 +0000

    ws

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,29 @@
-use paho_mqtt as mqtt;
-use std::{env, process, time::Duration};
-use log::{info, error, warn};
-use serde_json::Value;
-use futures::stream::select_all;
-use futures::{executor::block_on, stream::StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::protocol::Message;
+use futures_util::{StreamExt, SinkExt};
 
-const SUBS: &[&str] = &["rtl_433/Acurite-Atlas/622/msg5"];
-const QOS: &[i32] = &[mqtt::QOS_0];
+async fn handle_connection(stream: TcpStream) {
+    let ws_stream = accept_async(stream).await.expect("Error during the websocket handshake");
 
-// TODO tokio https://docs.rs/rumqttc/latest/rumqttc/
-// TODO ctrl+c (with tokio?)
-fn main() {
-    let _ec = |x: i32| -> i32 {
-        let yx = x + 1;
-        yx
-    };
-    
-    // Initialize the logger from the environment
-    env_logger::init();
-    
-    let host = env::args()
-    .nth(1)
-    .unwrap_or_else(|| "mqtt://homeassistant.local:1883".to_string());
-    
-    info!("Connecting to the MQTT broker at '{}'...", host);
-    
-    // Create the client. Use an ID for a persistent session.
-    // A real system should try harder to use a unique ID.
-    let create_opts = mqtt::CreateOptionsBuilder::new_v3()
-    .server_uri(host)
-    .client_id("rusty-async-subscriber")
-    .finalize();
-    
-    // Create the client connection
-    let mut cli = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
-        error!("Error creating the client: {:?}", e);
-        process::exit(1);
-    });
-    
-    if let Err(err) = block_on(async {
-        let strm = cli.get_stream(25);
-        
-        // Define the set of options for the connection
-        let _lwt = mqtt::Message::new(
-            "test/lwt",
-            "[LWT] Async subscriber lost connection",
-            mqtt::QOS_1,
-        );
-        
-        // Create the connect options, explicitly requesting MQTT v3.x
-        let conn_opts = mqtt::ConnectOptionsBuilder::new_v3()
-        .keep_alive_interval(Duration::from_secs(30))
-        .clean_session(false)
-        // .will_message(lwt)
-        .user_name("mqtt")
-        .password("mqtt")
-        .finalize();
-        
-        // Make the connection to the broker
-        cli.connect(conn_opts).await?;
-        
-        info!("Subscribing to topics: {:?}", SUBS);
-        cli.subscribe_many(SUBS, QOS).await?;
-        
-        let mut rconn_attempt: usize = 0;
-        let streams: Vec<_> = vec![strm];
-        
-        let mut fused_streams = select_all(streams);
-        while let Some(value) = fused_streams.next().await {
-            if let Some(msg) = value {
-                let s: String = msg.payload_str().into_owned();
-                info!("Received message: {:?}", msg.topic());
-                serde_json::from_str(&s).map(|json: Value| {
-                    json.get("humidity").map(|temp| {
-                        info!("humidity: {:?}", temp);
-                    }).unwrap_or_else(|| {
-                        error!("humidity not found");
-                    });
-                    
-                    json.get("temperature_F").map(|temp| {
-                        info!("temp(F): {:?}", temp);
-                    });
-                }).unwrap_or_else(|e| {
-                    error!("Error parsing JSON: {:?}", e);
-                });
-            } else {
-                // A "None" means we were disconnected. Try to reconnect...
-                warn!("Lost connection. Attempting reconnect...");
-                while let Err(err) = cli.reconnect().await {
-                    rconn_attempt += 1;
-                    warn!("Error reconnecting #{}: {}", rconn_attempt, err);
-                    // For tokio use: tokio::time::delay_for()
-                    async_std::task::sleep(Duration::from_secs(1)).await;
-                }
-                warn!("Reconnected.");
-            }
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(message) = read.next().await {
+        let message = message.expect("Error reading message");
+        if message.is_text() || message.is_binary() {
+            write.send(Message::Text("Hello from server".to_string())).await.expect("Error sending message");
         }
-        
-        // Explicit return type for the async block
-        Ok::<(), mqtt::Error>(())
-    }) {
-        warn!("{}", err);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(&addr).await.expect("Failed to bind");
+
+    println!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
     }
 }
