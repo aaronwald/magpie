0d239b701448218eca06eed89b2298fa7ed6a87f
Aaron Wald <aaronwald@gmail.com>
2024-12-01 19:35:59 -0500

    indent]

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ const SUBS: &[&str] = &["rtl_433/Acurite-Atlas/622/msg5"];
 
 async fn domqtt (ctx: Sender<String>) -> Result<()> {
     let host = "mqtt://homeassistant.local:1883".to_string();
-
+    
     info!("Connecting to the MQTT broker at '{}'...", host);
     
     // Create the client. Use an ID for a persistent session.
@@ -34,7 +34,7 @@ async fn domqtt (ctx: Sender<String>) -> Result<()> {
         error!("Error creating the client: {:?}", e);
         process::exit(1);
     });
-
+    
     let conn_opts = mqtt::ConnectOptionsBuilder::new_v3()
     .keep_alive_interval(Duration::from_secs(30))
     .clean_session(false)
@@ -46,28 +46,28 @@ async fn domqtt (ctx: Sender<String>) -> Result<()> {
         error!("Unable to connect: {:?}", e);
         process::exit(1);
     });
-
+    
     cli.subscribe_many(SUBS, QOS).await.unwrap_or_else(|e| {
         error!("Error subscribing to topics: {:?}", e);
         process::exit(1);
     });
     let strm = cli.get_stream(25);
     let streams: Vec<_> = vec![strm];
-        
-        
+    
+    
     let mut fused_streams = select_all(streams);
     while let Some(value) = fused_streams.next().await {
         if let Some(msg) = value {
             let s: String = msg.payload_str().into_owned();
             debug!("Received message: {:?} {:?}", msg.topic(), s);
-
+            
             if let Ok(json) = serde_json::from_str::<Value>(&s) {
                 if let Some(humidity) = json.get("humidity") {
                     debug!("humidity: {:?}", humidity);
                 } else {
                     error!("humidity not found");
                 }
-
+                
                 if let Some(temp) = json.get("temperature_F") {
                     let temp_str = format!("{:?}", temp);
                     debug!("temp(F): {:?}", temp_str);
@@ -81,7 +81,7 @@ async fn domqtt (ctx: Sender<String>) -> Result<()> {
             } else {
                 error!("Error parsing JSON: {:?}", s);
             }
-
+            
             // ctx.send(s.to_string()).await.expect("Error sending message");
         } else {
             warn!("Lost connection. Attempting reconnect...");
@@ -91,7 +91,7 @@ async fn domqtt (ctx: Sender<String>) -> Result<()> {
             });
         }
     }
-
+    
     Ok(())
 }
 
@@ -106,24 +106,24 @@ async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
 
 async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
     let ws_stream = accept_async(stream).await.expect("Error during the websocket handshake");
-
+    
     info!("New WebSocket connection: {}", peer);
-
+    
     let (mut write, mut read) = ws_stream.split();
     let (ctx, mut crx) = mpsc::channel(32);
     let cctx = ctx.clone();
-
+    
     tokio::spawn(async move {
         while let Some(message) = crx.recv().await {
             write.send(Message::Text(message)).await.expect("Error sending message");
             // TODO how to close channel?
         }
     });
-
+    
     tokio::spawn(async move {
         let _ = domqtt(cctx).await;
     });
-
+    
     while let Some(message) = read.next().await {
         let message = message.expect("Error reading message");
         if message.is_text() || message.is_binary() {
@@ -137,16 +137,16 @@ async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
 #[tokio::main]
 async fn main() {
     env_logger::init();
-
+    
     let addr = "0.0.0.0:8080";
     let listener = TcpListener::bind(&addr).await.expect("Failed to bind");
-
+    
     println!("Listening on: {}", addr);
-
+    
     while let Ok((stream, _)) = listener.accept().await {
         let peer = stream.peer_addr().expect("connected streams should have a peer address");
         info!("Peer address: {}", peer);
-
+        
         tokio::spawn(accept_connection(peer, stream));
     }
 }
